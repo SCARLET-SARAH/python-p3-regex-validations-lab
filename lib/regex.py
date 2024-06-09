db7038c7611dadf137ee617ee11a5194e23011ef
scarlet <sarahscarlet641@gmail.com>
2024-06-09 19:52:27 +0300

    complete

--- lib/regex.py
+++ lib/regex.py
@@ -4,10 +4,10 @@ import re
 # Feel free to encourage students to find oversights and add tests to this lab!
 
 name = r""
-name_regex = re.compile(name)
+name_regex =  re.compile(r"^[A-Za-z]+(?:['’][A-Za-z]+)?(?:-[A-Za-z]+(?:['’][A-Za-z]+)?)?\s[A-Za-z]+(?:['’][A-Za-z]+)?(?:-[A-Za-z]+(?:['’][A-Za-z]+)?)?$")
 
 phone_number = r""
-phone_regex = re.compile(phone_number)
+phone_regex =  re.compile(r'^\d{3}-\d{3}-\d{4}$')
 
 email_address = r""
-email_regex = re.compile(email_address)
+email_regex = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
